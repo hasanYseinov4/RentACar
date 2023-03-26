8a8cf2d4c26d123fea6f717a3e6163261f3488a6
hasanYseinov4 <your_github_email>
2023-03-26 16:48:34 +0300

    Hello

---
+++ Data/RentACarDBContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class RentACarDBContext : IdentityDbContext<User, IdentityRole, string>
+    {
+        DbSet<Car> Cars { get; set; }
+        DbSet<Request> Requests { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-6RR1DUB\\KELERMAN; Database=RentACarDataBase; Integrated Security=true");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            builder.Entity<Request>()
+                .HasOne(uc => uc.User)
+               .WithMany(r => r.Requests)
+               .HasForeignKey(r => r.UserId);
+
+            builder.Entity<Request>()
+                .HasOne(c => c.Car)
+                .WithMany(r => r.Requests)
+                .HasForeignKey(r => r.CarId);
+
+            builder.Entity<User>().HasIndex(u => u.Email).IsUnique();
+            builder.Entity<User>().HasIndex(u => u.UserName).IsUnique();
+            builder.Entity<User>().HasIndex(u => u.EGN).IsUnique();
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Data/User.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+
+    public class User : IdentityUser<string>
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string EGN { get; set; }
+        public bool IsAdimin { get; set; }
+        public IEnumerable<Request> Requests { get; set; }
+    }
+}
